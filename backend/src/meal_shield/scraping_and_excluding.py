f98ec8c9871fa7da1ac10f81c6e5d1e56c477bcc
okiijin <okii.jin@icloud.com>
2024-05-19 20:54:13 +0900

    Add file

---
+++ backend/src/meal_shield/scraping_and_excluding.py
@@ -0,0 +1,14 @@
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# WebDriverの設定
+import chromedriver_binary
+
+def scraping_and_excluding():
+
+def scraping():
+
+def excluding():
