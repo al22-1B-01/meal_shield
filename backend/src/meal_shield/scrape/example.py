d82b5d4fb51a18104b11a81d8ed9e89a7e97d9e7
iamtatsuki05 <tatsukio0522@gmail.com>
2024-05-22 00:13:32 +0900

    add scrape code

---
+++ backend/src/meal_shield/scrape/example.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+
+def main():
+    print('start')
+
+    # ChromeOptionsの設定
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # ヘッドレスモードで実行
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
+
+    driver = webdriver.Remote(
+        command_executor='http://selenium:4444/wd/hub',
+        options=options
+    )
+
+    driver.implicitly_wait(10)
+
+    url = 'https://datascience-beginer.com/'  # テストでアクセスするURLを指定
+    driver.get(url)
+
+    time.sleep(3)
+    driver.save_screenshot('test.png')  # アクセスした先でスクリーンショットを取得
+    driver.quit()
+
+if  __name__ == '__main__':
+    main()
+
