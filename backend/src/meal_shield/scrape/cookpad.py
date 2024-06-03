0ebc705b6f3e99874a91b9d10b8ef715e9ef5c0b
okiijin <okii.jin@icloud.com>
2024-06-04 01:12:25 +0900

    Update scraping_and_excluding.py and cookpad.py

---
+++ backend/src/meal_shield/scrape/cookpad.py
@@ -0,0 +1,114 @@
+import re
+from concurrent.futures import ThreadPoolExecutor
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def scraping_cookpad(recipe_name: str):
+
+    # 検索結果ページのURLリストを取得
+    page_urls = make_url_list(recipe_name)
+    if page_urls is None:
+        return None
+    # それぞれのページのURLからレシピのURLを並列処理で取得
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        recipe_url_lists = list(executor.map(scraping_recipe_url, page_urls))
+    # URLのリストを結合
+    recipe_url_list = [
+            item
+            for sublist in recipe_url_lists
+            for item in sublist
+            ]
+    # それぞれのレシピのURLからデータを並列処理で取得
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        recipe_data_list = list(
+                executor.map(scraping_recipe_data, recipe_url_list)
+                )
+    return recipe_data_list
+
+
+def make_url_list(recipe_name: str):
+
+    # 検索上限(page数)
+    limit_page = 10
+
+    #検索結果の最初のページのURL
+    url = f'https://cookpad.com/search/{recipe_name}'
+    response = requests.get(url)
+
+    # 検索結果が存在するとき
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'lxml')
+        # 1 / 1,000のような現在のページを表す文字列を取得
+        number_of_pages = soup.find(class_='number_of_pages').text
+        page_parts = number_of_pages.split(' / ')
+        sum_of_pages = int(page_parts[1].replace(",", ""))
+
+        # 検索上限を設定
+        if sum_of_pages > limit_page:
+            sum_of_pages = limit_page
+
+        url_list = []
+        # ページ数分だけURLを作成
+        for page_num in range(1, sum_of_pages + 1):
+            new_url = f'{url}?page={page_num}'
+            url_list.append(new_url)
+        return url_list
+    else:
+        return None
+
+
+def scraping_recipe_url(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    recipe_url_list = []
+    # レシピのURLを属性に持つ<a>タグをすべて取得
+    a_tags = soup.find_all('a', class_='recipe-title')
+    for a_tag in a_tags:
+        # 各<a>タグのhref属性(URL)を取得
+        href = a_tag.get('href')
+        # 会員登録が必要なレシピを除外
+        if 'dining' not in href:
+            recipe_url_list.append(f'https://cookpad.com{href}')
+    return recipe_url_list
+
+
+def scraping_recipe_data(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    # レシピのタイトルである<h1>タグのテキストを取得
+    recipe_title = soup.find('h1', class_='recipe-title') \
+    .get_text(strip=True)
+    # 全角スペースを半角スペースに置き換える
+    recipe_title = recipe_title.replace('\u3000', ' ')
+
+    ingredient_list = []
+    # 材料のデータを含む<span>タグの要素をすべて取得
+    spans = soup.find_all('span', class_='name')
+    for span in spans:
+        # 材料は<span>タグかその中の<a>タグにあるので場合分け
+        a_tag = span.find('a')
+        if a_tag is None:
+            ingredient_name = span.text
+        else:
+            ingredient_name = a_tag.text
+        ingredient_list.append(ingredient_name)
+
+    # レシピ画像のURLを属性に持つ<img>タグを含む<section>タグを取得
+    section_tag = soup.find('section', id='main-photo')
+    # レシピ画像のURLを属性に持つ<img>タグを取得
+    img_tag = section_tag.find('img')
+    # <img>タグのsrc属性(レシピ画像のURL)を取得
+    recipe_img_url = img_tag.get('src')
+
+    recipe_data = {
+            'recipe_title': recipe_title,
+            'ingredient_list': ingredient_list,
+            'recipe_url': url,
+            'recipe_img_url': recipe_img_url
+            }
+    return recipe_data
+
--- backend/src/meal_shield/scraping_and_excluding.py
+++ backend/src/meal_shield/scraping_and_excluding.py
@@ -1,14 +1,62 @@
-import time
-import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-# WebDriverの設定
-import chromedriver_binary
+from typing import List
 
-def scraping_and_excluding():
+from scrape import cookpad
 
-def scraping():
 
-def excluding():
+# テスト用main関数
+def main():
+    recipe_name = 'ココナッツカレー'
+    allergy_list = ['鶏', 'とり']
+
+    recipe_data_list = scraping_and_excluding(allergy_list, recipe_name)
+    
+    if recipe_data_list is not None:
+        for recipe_data in recipe_data_list:
+            print(f"{recipe_data['recipe_title']}")
+            print(f"{recipe_data['ingredient_list']}")
+            print(f"{recipe_data['recipe_url']}")
+            print(f"{recipe_data['recipe_img_url']}\n")
+        print(f'検索結果{len(recipe_data_list)}件')
+        print(f'検索レシピ名:{recipe_name}\n除外品目:\n{allergy_list}')
+    else:
+        print(recipe_data_list)
+        print('検索結果が見つかりませんでした')
+
+
+def scraping_and_excluding(allergy_list, recipe_name: str):
+    recipe_data_list = cookpad.scraping_cookpad(recipe_name)
+    if recipe_data_list is not None:
+        excluded_recipe_data_list = excluding(allergy_list, recipe_data_list)
+    else:
+        return recipe_data_list
+    return excluded_recipe_data_list
+
+
+# 文字列がリストに含まれる文字列を含むか判別
+def contains_any(string: str, substrings: List[str]) -> bool:
+    return any(substring in string for substring in substrings)
+
+
+# 文字列リストにリストの文字列が含まれるか判別
+def contains_any_in_list(strings: List[str], substrings: List[str]) -> bool:
+    # リストの要素の判別結果をboolリストに格納
+    results = [contains_any(string, substrings) for string in strings]
+    for result in results:
+        if result:
+            return True
+    return False
+
+
+def excluding(allergy_list: List[str], recipe_data_list: List[dict]):
+    # 材料にアレルギーを含む要素を除外
+    excluded_recipe_data_list = [
+        recipe_data for recipe_data in recipe_data_list
+        if not contains_any_in_list(
+            recipe_data['ingredient_list'], allergy_list
+        )
+    ]
+    return excluded_recipe_data_list
+
+
+if __name__ == '__main__':
+    main()
