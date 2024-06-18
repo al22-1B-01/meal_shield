0375136c26042e1a3b1a3bf726132b1e1844a3a3
yuseitake <yuusei0625@icloud.com>
2024-06-18 16:02:41 +0900

    ページ遷移の修正

--- frontend/src/meal_shield/app.py
+++ frontend/src/meal_shield/app.py
@@ -1,7 +1,17 @@
-from search import search_recipe_entrypoint
+import streamlit as st
+from search import search_recipe_entrypoint, show_search_results
+from detail import show_details
 
+def main():
+    if 'page' not in st.session_state:
+        st.session_state.page = '検索'
 
+    if st.session_state.page == '検索':
+        search_recipe_entrypoint()
+    elif st.session_state.page == '検索結果':
+        show_search_results()
+    elif st.session_state.page == '詳細':
+        show_details()
 
-if __name__ == '__main__':
-    search_recipe_entrypoint()
-
+if __name__ == "__main__":
+    main()
