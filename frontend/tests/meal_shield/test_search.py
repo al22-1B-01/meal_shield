b9cde4374591d50d72e0c7a6a9c1a6ed84c1c383
yuseitake <yuusei0625@icloud.com>
2024-06-27 17:33:30 +0900

    black

--- frontend/tests/meal_shield/test_search.py
+++ frontend/tests/meal_shield/test_search.py
@@ -97,26 +97,26 @@ def search_recipe_entrypoint() -> None:
     recipe_name = st.text_input('レシピ名を入力してください')
 
     if st.button('検索'):
-        #recipes = fetch_recipes(recipe_name, st.session_state.allergy_list)
-        #テストようのrecipesの作成
+        # recipes = fetch_recipes(recipe_name, st.session_state.allergy_list)
+        # テストようのrecipesの作成
         recipes = [
-        {
-            'recipe_title': 'イチゴケーキ',
-            'recipe_image_url': 'https://img.cpcdn.com/recipes/7813040/'
-            '894x1461s/952f6a9105c7b1d250853791cc4b08fd?u=117'
-            '56033&p=1714165191',
-            'recipe_ingredients': ['卵', '砂糖', 'バター'],
-            'recipe_url': 'https://cookpad.com/recipe/7813040',
-        },
-        {
-            'recipe_title': 'いちごけーき',
-            'recipe_image_url': 'https://img.cpcdn.com/recipes/7781284/'
-            '894x1461s/9b37148a4668a565830b9514d3af1a74?u=9240495'
-            '&p=1711003018',
-            'recipe_ingredients': ['卵', '牛乳', 'バニラ'],
-            'recipe_url': 'https://cookpad.com/recipe/7781284',
-        },
-    ]
+            {
+                'recipe_title': 'イチゴケーキ',
+                'recipe_image_url': 'https://img.cpcdn.com/recipes/7813040/'
+                '894x1461s/952f6a9105c7b1d250853791cc4b08fd?u=117'
+                '56033&p=1714165191',
+                'recipe_ingredients': ['卵', '砂糖', 'バター'],
+                'recipe_url': 'https://cookpad.com/recipe/7813040',
+            },
+            {
+                'recipe_title': 'いちごけーき',
+                'recipe_image_url': 'https://img.cpcdn.com/recipes/7781284/'
+                '894x1461s/9b37148a4668a565830b9514d3af1a74?u=9240495'
+                '&p=1711003018',
+                'recipe_ingredients': ['卵', '牛乳', 'バニラ'],
+                'recipe_url': 'https://cookpad.com/recipe/7781284',
+            },
+        ]
 
         st.session_state.recipes = recipes
         st.session_state.page = '検索結果'
