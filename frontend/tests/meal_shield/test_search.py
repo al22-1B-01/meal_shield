6d7f2054b78368ed3a7996aa529e347cee4bd507
iamtatsuki05 <tatsukio0522@gmail.com>
2024-07-04 04:49:16 +0900

    update page mock

--- frontend/tests/meal_shield/test_search.py
+++ frontend/tests/meal_shield/test_search.py
@@ -145,6 +145,7 @@ def test_show_details_page(mock_get, mock_session_state):
 @patch('streamlit.session_state', new_callable=MagicMock)
 def test_validate_input_data_no_recipe_name(mock_session_state, mock_error):
     mock_session_state.recipes = [{'status':  'error'}]
+    mock_session_state.page = '検索結果'
     validate_input_data('', ['卵', '牛乳'])
     mock_error.assert_any_call('レシピが入力されていません.')
 
@@ -153,6 +154,7 @@ def test_validate_input_data_no_recipe_name(mock_session_state, mock_error):
 @patch('streamlit.session_state', new_callable=MagicMock)
 def test_validate_input_data_no_allergies_list(mock_session_state, mock_error):
     mock_session_state.recipes = [{'status':  'error'}]
+    mock_session_state.page = '検索結果'
     validate_input_data('ケーキ', [])
     mock_error.assert_any_call('アレルギー品目が入力されていません.')
 
@@ -161,6 +163,7 @@ def test_validate_input_data_no_allergies_list(mock_session_state, mock_error):
 @patch('streamlit.session_state', new_callable=MagicMock)
 def test_validate_input_data_no_recipes(mock_session_state, mock_error):
     mock_session_state.recipes = [{'status':  'error'}]
+    mock_session_state.page = '検索結果'
     validate_input_data('ケーキ', ['卵', '牛乳'])
     mock_error.assert_any_call('検索結果が存在しません.')
 
@@ -169,5 +172,6 @@ def test_validate_input_data_no_recipes(mock_session_state, mock_error):
 @patch('streamlit.session_state', new_callable=MagicMock)
 def test_validate_input_data_valid_input(mock_session_state, mock_error):
     mock_session_state.recipes = [{'status':  'success'}]
+    mock_session_state.page = '検索結果'
     validate_input_data('ケーキ', ['卵', '牛乳'])
     mock_error.assert_not_called()
