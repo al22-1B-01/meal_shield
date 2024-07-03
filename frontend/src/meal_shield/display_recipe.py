dc5689e129c5592a69faeb4aa3580d9edba42fcc
okytmoka <yamatoya0071@gmail.com>
2024-07-03 09:14:18 +0900

    display_recipe変更

--- frontend/src/meal_shield/display_recipe.py
+++ frontend/src/meal_shield/display_recipe.py
@@ -23,15 +23,6 @@ def get_recipe_summary(
     st.markdown(box_style.format(content=selected_list), unsafe_allow_html=True)
     st.title('料理名')
     st.markdown(box_style.format(content=recipe_name), unsafe_allow_html=True)
-
-    make_recipe_summary(allergy_list, recipe_name, recipes)
-
-
-def make_recipe_summary(
-    allergy_list: list[str],
-    recipe_name: str,
-    recipes: list[dict[str, Union[str, list[str]]]],
-) -> None:
     button_css = """
     <style>
     dic.stButton > button:first-child  {{
@@ -42,12 +33,25 @@ def make_recipe_summary(
     </style>
     """
     for idex, item in enumerate(recipes):
+        new_recipe, recipe_img_url = make_recipe_summary(
+            allergy_list, recipe_name, item
+        )
         button_key = f'{recipe_name}_{idex}'
-        st.write(f"{idex + 1}位  {item['recipe_title']}")
+        st.write(f"{idex + 1}位  {new_recipe['recipe_title']}")
         st.markdown(button_css, unsafe_allow_html=True)
-        st.image(item['recipe_image_url'], width=300)
+        st.image(recipe_img_url, width=300)
 
-        if st.button(f"{item['recipe_title']}", key=button_key):
+        if st.button(f"{new_recipe['recipe_title']}", key=button_key):
             st.session_state.page = 'details'
-            st.session_state.selected_item = item
+            st.session_state.selected_item = new_recipe
             st.rerun()  # ページをリロードして変更を反映
+
+
+def make_recipe_summary(
+    allergy_list: list[str],
+    recipe_name: str,
+    recipes: list[dict[str, Union[str, list[str]]]],
+) -> tuple[dict[str, Union[str, list[str]]], str]:
+    recipe_img_url = recipes.get('recipe_image_url')
+    recipes['allergy_list'] = [allergy_list]
+    return recipes, recipe_img_url
