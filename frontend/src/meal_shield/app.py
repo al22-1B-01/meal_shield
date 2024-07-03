961ead90a18679139403dae7ff1e2353f4817a33
yuseitake <yuusei0625@icloud.com>
2024-07-03 12:11:57 +0900

    関数追加

--- frontend/src/meal_shield/app.py
+++ frontend/src/meal_shield/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from meal_shield.detail import show_details
 from meal_shield.display_recipe import display_recipe
-from meal_shield.search import search_recipe_entrypoint
+from meal_shield.search import search_recipe_entrypoint, validate_input_data
 
 
 def main():
@@ -10,17 +10,15 @@ def main():
         search_recipe_entrypoint()
 
     elif st.session_state.page == '検索結果':
-        if not st.session_state.recipe_name:
-            st.warning('レシピ名を入力してください。')
-            search_recipe_entrypoint()
-        elif not st.session_state.recipes:
-            st.write('レシピが見つかりませんでした。')
-        else:
-            display_recipe(
-                allergy_list=st.session_state.allergy_list,
-                recipe_name=st.session_state.recipe_name,
-                recipes=st.session_state.recipes,
-            )
+        validate_input_data(
+            recipe_name=st.session_state.recipe_name,
+            allergies_list=st.session_state.allergy_list
+        )
+        display_recipe(
+            allergy_list=st.session_state.allergy_list,
+            recipe_name=st.session_state.recipe_name,
+            recipes=st.session_state.recipes,
+        )
 
     elif st.session_state.page == 'details':
         show_details()
