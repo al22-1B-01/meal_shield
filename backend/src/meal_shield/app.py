012bef3e6574884903964a788b0622b94297ae67
okytmoka <yamatoya0071@gmail.com>
2024-07-09 16:23:14 +0900

    lint

--- backend/src/meal_shield/app.py
+++ backend/src/meal_shield/app.py
@@ -6,7 +6,7 @@
     serch_allergyで詳細情報を取得
     get_recipeでレシピを取得
     anking_recipeはレシピに順位付けしたもの
-    scraping_and_excluding：cookpadからレシピをスクレイピング
+    scraping_and_excluding:cookpadからレシピをスクレイピング
     
 """
 from typing import Final, Optional
@@ -54,13 +54,13 @@ WORDS: Final[dict[str, list[str]]] = {
 
 def serch_allergy(allergy_list: list[str]) -> list[list[str]]:
     """概要
-    
+
     説明
     param:allergy_list;選択されたアレルギー品目
     type:a;;ergy_list:list[str]
     return:WORDSからallergy_listから取得
     rtype:list[list[str]]
-    
+
     """
     allergu_search = []
     for item in allergy_list:
@@ -74,7 +74,7 @@ async def get_recipe(
     recipe: str, allergy_list: Optional[list[str]] = Query(default=None)
 ) -> list:
     """概要
-    
+
     説明
     param:recipe:入力されたレシピ
     type:recipe:str
@@ -82,7 +82,7 @@ async def get_recipe(
     type:list
     return:順位付けされたレシピをかえす
     rtype:list[dict[str, Union[str, list[str]]]]
-    
+
     """
     if allergy_list is None:
         return [{'status': 'error', 'message': 'No allergy list', 'data': []}]
